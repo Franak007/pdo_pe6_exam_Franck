2e74cf5cc9be40b8eb78195e183bf73694eca01b
Franak007 <franck.pertosa@gmail.com>
2023-08-02 14:24:39 +0200

    PDOService installé

--- src/Service/PDOService.php
+++ src/Service/PDOService.php
@@ -8,8 +8,32 @@ class PDOService
 {
     private PDO $PDO;
 
-    private string $dsn;
-    private string $user;
-    private string $password;
+    private string $dsn = 'mysql:host=127.0.0.1:3306;dbname=pdo_exam';
+    private string $user = "root";
+    private string $password = '';
 
+    public function __construct()
+    {
+        $this->pdo = new pdo($this->dsn, $this->user, $this->password);
+    }
+
+    public function getPDO(): pdo
+    {
+        return $this->pdo;
+    }
+
+    public function getDsn(): string
+    {
+        return $this->dsn;
+    }
+
+    public function getUser(): string
+    {
+        return $this->user;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
 }
